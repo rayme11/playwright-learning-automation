fbc246add92ddd5a26bb986902418e954070a323
RayMaldonadoEs <ray.maldonado@kellerhome.com>
2025-12-16 12:12:51 -0600

    Refactor search test to use test.describe and test.beforeEach for better structure

--- tests/kw-search.spec.ts
+++ tests/kw-search.spec.ts
@@ -1,11 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://kw.com/');
-  await page.getByRole('textbox', { name: 'City, Neighborhood, Address,' }).click();
-  await page.getByRole('textbox', { name: 'City, Neighborhood, Address,' }).fill('austin');
-  await page.getByText('Austin, TX, USA', { exact: true }).click();
-  await page.goto('https://kw.com/search/sale/Austin-TX-USA/929288?q=Austin%2C+TX%2C+USA&viewport=30.60196343072092%2C-97.292110675%2C29.990125873260578%2C-98.171016925');
-  await expect(page.locator('.gm-style > div > div:nth-child(2)')).toBeVisible();
-  await expect(page.locator('flutter:Text("Properties for Sale")')).toBeVisible();
-  // await expect(page.locator('flutter:BySemanticsLabel("Properties for Sale")')).toBeVisible();});
+test.describe('Home Page Test Suite', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://kw.com/');
+  });
+
+  test('Load the home page and do simple search', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'City, Neighborhood, Address,' }).click();
+    await page.getByRole('textbox', { name: 'City, Neighborhood, Address,' }).fill('austin');
+    await page.getByText('Austin, TX, USA', { exact: true }).click();
+    await page.goto('https://kw.com/search/sale/Austin-TX-USA/929288?q=Austin%2C+TX%2C+USA&viewport=30.60196343072092%2C-97.292110675%2C29.990125873260578%2C-98.171016925');
+    await expect(page.locator('.gm-style > div > div:nth-child(2)')).toBeVisible();
+    // await expect(page.locator('flutter:BySemanticsLabel("Properties for Sale")')).toBeVisible();
+  });
+});
