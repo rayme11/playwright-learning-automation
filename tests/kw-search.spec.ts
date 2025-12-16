a44fb69b1c2dd9618204ac5b1a827062da04546f
RayMaldonadoEs <ray.maldonado@kellerhome.com>
2025-12-16 11:47:46 -0600

    Comment out redundant visibility check for properties in search test

--- tests/kw-search.spec.ts
+++ tests/kw-search.spec.ts
@@ -8,4 +8,4 @@ test('test', async ({ page }) => {
   await page.goto('https://kw.com/search/sale/Austin-TX-USA/929288?q=Austin%2C+TX%2C+USA&viewport=30.60196343072092%2C-97.292110675%2C29.990125873260578%2C-98.171016925');
   await expect(page.locator('.gm-style > div > div:nth-child(2)')).toBeVisible();
   await expect(page.locator('flutter:Text("Properties for Sale")')).toBeVisible();
-  await expect(page.locator('flutter:BySemanticsLabel("Properties for Sale")')).toBeVisible();});
+  // await expect(page.locator('flutter:BySemanticsLabel("Properties for Sale")')).toBeVisible();});
